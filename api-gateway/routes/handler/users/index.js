740b7c4dfacb294c07df49717065dee8a4272b53
dimasadh <dimasadh24@gmail.com>
2023-08-20 17:33:04 +0700

    integrasi service getUser into gateway

--- api-gateway/routes/handler/users/index.js
+++ api-gateway/routes/handler/users/index.js
@@ -1,7 +1,8 @@
 const register = require('./register');
 const login = require('./login');
 const update = require('./update');
+const getUser = require('./getUser');
 
 module.exports = {
-    register, login, update
+    register, login, update, getUser
 }
