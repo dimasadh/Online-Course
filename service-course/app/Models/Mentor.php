62255c0fbcc57fc272cc54ae7b95bf9efaaa3299
dimasadh <dimasadh24@gmail.com>
2023-10-14 11:13:54 +0700

    Add initial Models

---
+++ service-course/app/Models/Mentor.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mentor extends Model
+{
+    use HasFactory;
+
+    protected $table = 'mentor';
+    protected $fillable = [
+        'name', 'profile', 'email', 'profession'
+    ];
+}
