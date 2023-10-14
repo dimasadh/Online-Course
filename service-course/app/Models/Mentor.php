ddd1b0871652a315308daf68ef6b002c2bff42e8
dimasadh <dimasadh24@gmail.com>
2023-10-14 11:59:11 +0700

    Add API create Mentor

--- service-course/app/Models/Mentor.php
+++ service-course/app/Models/Mentor.php
@@ -9,8 +9,12 @@ class Mentor extends Model
 {
     use HasFactory;
 
-    protected $table = 'mentor';
+    protected $table = 'mentors';
     protected $fillable = [
         'name', 'profile', 'email', 'profession'
+    ];
+    protected $casts = [
+        'created_at' => 'datetime: Y-m-d H:m:s',
+        'updated_at' => 'datetime: Y-m-d H:m:s'
     ];
 }
